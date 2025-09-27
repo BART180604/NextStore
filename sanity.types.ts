4929a390aa054c7f43d5572e3592741dbc14be2a
BART180604 <mbatamoussi81@gmail.com>
2025-09-27 01:37:44 +0100

    sanityTypes

--- sanity.types.ts
+++ sanity.types.ts
@@ -292,71 +292,8 @@ export type CATEGORIES_QUERIESResult = Array<{
   };
 }>;
 // Variable: MY_ORDERS_QUERY
-// Query: *[_type=="order" && clerkUserId==$userId ] | order(orderDAta desc){        ...,products[]{            ...,product->        }    }
-export type MY_ORDERS_QUERYResult = Array<{
-  _id: string;
-  _type: "order";
-  _createdAt: string;
-  _updatedAt: string;
-  _rev: string;
-  orderNumber?: string;
-  invoice?: {
-    id?: string;
-    number?: string;
-    hosted_invoice_url?: string;
-  };
-  stripeCheckoutSessionId?: string;
-  stripeCustomerId?: string;
-  clerkUserId?: string;
-  customerName?: string;
-  email?: string;
-  stripePaymentIntentId?: string;
-  products: Array<{
-    product: {
-      _id: string;
-      _type: "product";
-      _createdAt: string;
-      _updatedAt: string;
-      _rev: string;
-      name?: string;
-      slug?: Slug;
-      image?: Array<{
-        asset?: {
-          _ref: string;
-          _type: "reference";
-          _weak?: boolean;
-          [internalGroqTypeReferenceTo]?: "sanity.imageAsset";
-        };
-        media?: unknown;
-        hotspot?: SanityImageHotspot;
-        crop?: SanityImageCrop;
-        _type: "image";
-        _key: string;
-      }>;
-      intro?: string;
-      description?: string;
-      price?: number;
-      discount?: number;
-      category?: Array<{
-        _ref: string;
-        _type: "reference";
-        _weak?: boolean;
-        _key: string;
-        [internalGroqTypeReferenceTo]?: "category";
-      }>;
-      stock?: number;
-      status?: "hot" | "new" | "sale";
-      variants?: "hoodie" | "jacket" | "others" | "pants" | "short" | "tshirt";
-    } | null;
-    quantity?: number;
-    _key: string;
-  }> | null;
-  totalPrice?: number;
-  currency?: string;
-  amountDiscount?: number;
-  status?: "cancelled" | "delivered" | "paid" | "pending" | "shipped";
-  orderDate?: string;
-}>;
+// Query: *[_type=="order" && clerkUserId==$userId ] | order(orderDAta desc){        ...products[]{            ...product->        }    }
+export type MY_ORDERS_QUERYResult = Array<object>;
 
 // Query TypeMap
 import "@sanity/client";
@@ -364,6 +301,6 @@ declare module "@sanity/client" {
   interface SanityQueries {
     "*[_type =='product' && slug.current == $slug] | order(name asc) [0]": PRODUCT_BY_SLUG_QUERYResult;
     "*[_type=='category'] | order(title asc)": CATEGORIES_QUERIESResult;
-    "*[_type==\"order\" && clerkUserId==$userId ] | order(orderDAta desc){\n        ...,products[]{\n            ...,product->\n        }\n    }": MY_ORDERS_QUERYResult;
+    "*[_type==\"order\" && clerkUserId==$userId ] | order(orderDAta desc){\n        ...products[]{\n            ...product->\n        }\n    }": MY_ORDERS_QUERYResult;
   }
 }
